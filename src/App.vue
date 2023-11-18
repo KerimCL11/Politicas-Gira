78b6f672daac6d727898478f270d539284e17235
juan gila <juan.vargas@gilasw.com>
2023-11-18 17:18:40 -0600

    cambio de texto, estilos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="politica-de-privacidad">
+      <h1>Gira Gira</h1>
+
+      <h2>Política de Privacidad</h2>
+      <p>Bienvenido a nuestra aplicación. Esta aplicación no recopila ni utiliza información personal de los usuarios.</p>
+
+      <h3>Información recopilada</h3>
+      <p>Nuestra aplicación no recopila ningún tipo de información personal del usuario. No almacenamos datos, no accedemos a la cámara ni al micrófono, y no utilizamos ninguna funcionalidad que requiera el acceso a información sensible.</p>
+
+      <h3>Uso de la información</h3>
+      <p>Como no recopilamos información personal, no utilizamos ni compartimos ninguna información con terceros. La aplicación está diseñada para ser simple y no tiene funciones que requieran el uso de datos del usuario.</p>
+
+      <h3>Privacidad del usuario</h3>
+      <p>La privacidad del usuario es nuestra prioridad. No rastreamos ni almacenamos ninguna información que pueda identificar a los usuarios. La aplicación se centra en proporcionar una experiencia de usuario sencilla y divertida.</p>
+
+      <h3>Contacto</h3>
+      <p>Si tienes alguna pregunta sobre nuestra política de privacidad, no dudes en contactarnos en juanpivl@hotmail.com.</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
 
 <style>
 #app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh; /* Ocupa toda la altura de la ventana */
+}
+
+#politica-de-privacidad {
+  max-width: 600px; /* Ajusta según el diseño deseado */
+  text-align: justify;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,26 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<template id="politica-de-privacidad">
+  <div>
+    <h1 style="text-align: center;">Gira Gira</h1>
+
+    <h2 style="text-align: center;">Política de Privacidad</h2>
+    <p>Bienvenido a nuestra aplicación. Esta aplicación no recopila ni utiliza información personal de los usuarios.</p>
+
+    <h3>Información recopilada</h3>
+    <p>Nuestra aplicación no recopila ningún tipo de información personal del usuario. No almacenamos datos, no accedemos a la cámara ni al micrófono, y no utilizamos ninguna funcionalidad que requiera el acceso a información sensible.</p>
+
+    <h3>Uso de la información</h3>
+    <p>Como no recopilamos información personal, no utilizamos ni compartimos ninguna información con terceros. La aplicación está diseñada para ser simple y no tiene funciones que requieran el uso de datos del usuario.</p>
+
+    <h3>Privacidad del usuario</h3>
+    <p>La privacidad del usuario es nuestra prioridad. No rastreamos ni almacenamos ninguna información que pueda identificar a los usuarios. La aplicación se centra en proporcionar una experiencia de usuario sencilla y divertida.</p>
+
+    <h3>Contacto</h3>
+    <p>Si tienes alguna pregunta sobre nuestra política de privacidad, no dudes en contactarnos en privacy@example.com.</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
